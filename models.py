0d04dfe3c4e93076ddcdce0fb121d37c21377f44
Jero Sanchez <dev@jerosanchez.com>
2025-10-13 15:05:28 +0200

    Improve Post model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from database import Base
 
@@ -11,5 +12,6 @@ class Post(Base):
     id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="TRUE")
     rating = Column(Integer, nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True), server_default="now()")
