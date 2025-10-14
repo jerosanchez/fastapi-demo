8aac9e9ac92ba5f07221249b050b6245b8385c31
Jero Sanchez <dev@jerosanchez.com>
2025-10-14 11:37:35 +0200

    Refactor login to use form params instead of json

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.auth.oauth2 import create_access_token
@@ -12,8 +13,12 @@ router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-def login(credentials: UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == credentials.email).first()
+def login(
+    credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = (
+        db.query(models.User).filter(models.User.email == credentials.username).first()
+    )
 
     if not user:
         _report_invalid_login()
