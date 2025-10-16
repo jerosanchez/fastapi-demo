b838c278d029bc49bc261bd04b4b7360f23e7de8
Jero Sanchez <dev@jerosanchez.com>
2025-10-16 18:38:52 +0200

    Add CORS basic config

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import auth, posts, users, votes
 from app.core.database import engine
 
 app = FastAPI()
+
+# Allowed origins for CORS
+allowed_origins = [
+    "*",  # Allows all origins, maybe too permissive for production
+    # "https://www.google.com",  # Example of an allowed origin, adjust as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 posts.init_service(app, engine)
 users.init_service(app, engine)
