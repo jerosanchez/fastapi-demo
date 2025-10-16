1540fd3dcb13c916ddafcfffb705a19bd58b2cb4
Jero Sanchez <dev@jerosanchez.com>
2025-10-16 18:45:50 +0200

    Fix isort lint warnings

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 # Import all model classes here so Alembic can discover them
-from .users.models import User
 from .posts.models import Post
+from .users.models import User
 from .votes.models import Vote
 
 __all__ = ["User", "Post", "Vote"]
