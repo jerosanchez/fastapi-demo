e45e0ed72bc76b678ed77e1ed99e3712dad50a19
Jero Sanchez <dev@jerosanchez.com>
2025-10-17 18:56:57 +0200

    Remove hardcoded values in config

--- app/core/config.py
+++ app/core/config.py
@@ -7,22 +7,14 @@ class Settings(BaseSettings):
     # --- database
     database_url: str
 
-    # We set default values here on purpose, but they should be overridden
-    # by environment variables, specially the oauth configuration.
-    # They are required for migration to run properly in its own container.
-
-    # TODO: Improve this setup to avoid hardcoding secrets in code.
-
     # --- auth
-    oauth_hash_key: str = (
-        "7bdb4bcf0b01f00ca206693965b29145611d9419563630525dc848024c38fc08"
-    )
-    oauth_algorithm: str = "HS256"
-    oauth_token_ttl: int = 30  # in minutes
+    oauth_hash_key: str
+    oauth_algorithm: str
+    oauth_token_ttl: int
 
     # --- pagination
-    default_page_size: int = 5
-    max_page_size: int = 20
+    default_page_size: int
+    max_page_size: int
 
     class Config:
         env_file = (".env",) if os.path.exists(".env") else None
